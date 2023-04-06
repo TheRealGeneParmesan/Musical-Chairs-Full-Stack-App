19b5b67b615b8e5d2be47a9e79fd09f8cabba4d0
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-04-06 15:26:45 -0700

    last formatting change with npm run format

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,20 +1,19 @@
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
     const response = await fetch(`/api/post/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to delete post');
+      alert("Failed to delete post");
     }
   }
 };
 
 document
-  .querySelector('.post-list')
-  .addEventListener('click', delButtonHandler);
-
+  .querySelector(".post-list")
+  .addEventListener("click", delButtonHandler);
